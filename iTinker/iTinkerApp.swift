7f832cea02ac34eeddde509a0a7e741d54ac1db2
ext2txe <ext2txe@gmail.com>
2024-04-26 08:58:07 +0200

    Initial Commit

---
+++ iTinker/iTinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTinkerApp.swift
+//  iTinker
+//
+//  Created by Dzing on 26.04.24.
+//
+
+import SwiftUI
+
+@main
+struct iTinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
